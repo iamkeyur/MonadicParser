edd6057f80bea1ae4db92385c24196e7cbf30578
Keyur <keyur13@gecg28.ac.in>
2020-04-08 22:57:18 -0400

    Added main.ml

---
+++ main.ml
@@ -0,0 +1,44 @@
+(* This is an OCaml editor.
+   Enter your program here and send it to the toplevel using the "Eval code"
+   button. *)
+
+type 'a result =
+  | Success of 'a
+  | Failure of string
+  
+let explode s =
+  let rec expl i l =
+    if i < 0 then l else
+      expl (i - 1) (s.[i] :: l) in
+  expl (String.length s - 1) [];;
+
+let implode l =
+  let result = Bytes.create (List.length l) in 
+  let rec imp i = function
+    | [] -> result
+    | c :: l -> Bytes.set result i c; imp (i + 1) l in
+  imp 0 l;;
+
+(*
+  let pchar charToMatch str =
+    match explode str with
+    | [] -> Failure "Empty"
+    | h::t -> if h = charToMatch
+        then Success(charToMatch, implode t)
+        else Failure "Not Found";;
+*)
+
+let pchar charToMatch str =
+  match explode str with
+  | [] -> Failure "Empty"
+  | h::t -> match compare h charToMatch with
+    | 0 -> Success(charToMatch, implode t)
+    | _ -> Failure "Not Found";;
+             
+             
+let inputZBC = "ABC";; 
+pchar 'A' inputZBC;;
+
+let parseB = pchar 'B';;
+
+parseB "BC";;
